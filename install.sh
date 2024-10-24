9777159eb463d21616ccbc62dcb58ac1f3f10efc
eXkrae <exkrae@gmail.com>
2024-10-24 17:18:02 +0300

    Update files

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing programs..."
-sudo pacman -S xorg firefox vim alacritty mc btop cmake smplayer cmus numlockx udiskie pamixer flameshot dmenu picom feh zip unzip
+sudo pacman -S xorg firefox neovim alacritty mc htop cmake smplayer cmus numlockx udiskie pamixer flameshot dmenu picom feh zip unzip
 
 echo "Installing fonts..."
 sudo pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
